76466ea286e9dacef894839f3cd07a49025e6cf9
Miacarmen <maitreyacarmen@gmail.com>
2022-09-11 17:51:45 -0700

    fixed server to run on different port

--- server/server.js
+++ server/server.js
@@ -4,25 +4,32 @@ const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware for parsing json data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build/index.html')));
-}
+// if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build/index.html')));
+// }
 
-app.use(routes);
+// app.use(routes);
 
-// // Routes
-// // send the index.html file always
-// // app.get("/*", (req, res) => {
-// //     res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// //   });
+// Routes
+// send the index.html file always
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
-// Start Node + Express server on Port 5000
-db.once('open', () => {
-    app.listen(PORT, () => console.log(`App running on localhost: ${PORT}`));
-});
+// Start Node + Express server on Port 4000
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`App running on localhost: ${PORT}`);
+//   });
+// });
+
+app.listen(PORT, () => {
+    console.log(`App running on localhost: ${PORT}`);
+  });
